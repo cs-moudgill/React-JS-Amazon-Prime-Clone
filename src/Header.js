18a024acaa9f8a0c87f4c39425fde3ec1ca45e5b
cs-moudgill <channpreet.moudgill@gmail.com>
2021-03-06 21:58:03 -0400

    initial commit

---
+++ src/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Header.css";
+
+function Header() {
+  return (
+    <div className="header">
+      <div className="nav-items">
+        <img src="/public/images/amazon-prime-logo.png" alt="" />
+      </div>
+    </div>
+  );
+}
+
+export default Header;
